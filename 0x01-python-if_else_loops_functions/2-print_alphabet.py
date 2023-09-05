7bd2ac65a1bb07f61ac4f977c6422f6fea869526
0ussamabernou <oussama_bernou@um5.ac.ma>
2023-09-05 14:09:22 +0100

    fixed 2 print

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -3,4 +3,4 @@ for i in range(26):
     a = ord("a")
     if (a + i) == ord("e") or (a + i) == ord("q"):
         continue
-    print(f"{chr(a+i)}", end="")
+    print("{}".format(chr(a + i)), end="")
