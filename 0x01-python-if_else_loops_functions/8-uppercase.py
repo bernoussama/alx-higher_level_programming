4bc8a7cf61462f68c014a313a5ea0d1bd3111d0f
0ussamabernou <oussama_bernou@um5.ac.ma>
2023-09-05 22:00:16 +0100

    update

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,5 +5,6 @@ islower = __import__("7-islower").islower
 def uppercase(str):
     offset = ord("A") - ord("a")
     for char in str:
-        print("{}".format(chr(ord(char) + offset) if islower(char) else char), end="")
+        char = chr(ord(char) + offset) if islower(char) else char
+        print("{}".format(char), end="")
     print()
