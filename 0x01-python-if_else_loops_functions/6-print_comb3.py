e58a6fa98e6883a7b3dd2165dc231edf444fb202
0ussamabernou <oussama_bernou@um5.ac.ma>
2023-09-05 21:38:13 +0100

    update task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@ k = 1
 for i in range(10):
     for j in range(k, 10):
         if i == 8 and j == 9:
-            print("{:d}{:d},".format(i, j))
+            print("{:d}{:d}".format(i, j))
             break
         if i != j:
             print("{:d}{:d},".format(i, j), end=" ")
