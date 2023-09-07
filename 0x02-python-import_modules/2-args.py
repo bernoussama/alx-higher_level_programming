a8fecac03ab5aa60f42fc0fbb3dc7a0c1eea37bc
0ussamabernou <oussama_bernou@um5.ac.ma>
2023-09-07 18:52:35 +0100

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    argc = len(argv) - 1
+    if argc > 0:
+        print(f"{argc} arguments:")
+    else:
+        print("0 arguments.")
+
+    for idx, arg in enumerate(argv):
+        if idx != 0:
+            print(f"{idx}:{arg}")
