c8e69e376d7f4d9720e3803d484f5de7c72ac5b8
0ussamabernou <oussama_bernou@um5.ac.ma>
2023-09-07 18:40:59 +0100

    update

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,23 +2,10 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
 
-    def printop(a: int, b: int, op):
-        operation: str = ""
-        match op.__name__:
-            case "add":
-                operation = "+"
-            case "sub":
-                operation = "-"
-            case "mul":
-                operation = "*"
-            case "div":
-                operation = "/"
-
-        print("{} {} {} = {}".format(a, operation, b, op(a, b)))
-
     a = 10
     b = 5
-    printop(a, b, add)
-    printop(a, b, sub)
-    printop(a, b, mul)
-    printop(a, b, div)
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
