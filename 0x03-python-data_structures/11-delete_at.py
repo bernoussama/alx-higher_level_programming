5914965b744cf207800de38ffcd5490fdff792a7
0ussamabernou <oussama_bernou@um5.ac.ma>
2023-09-11 22:37:51 +0100

    update

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def delete_at(my_list=[], idx=0):
+    if idx > 0 and idx < len(my_list):
+        new_list = my_list
+        del new_list[idx]
+        return new_list
+    else:
+        return my_list
