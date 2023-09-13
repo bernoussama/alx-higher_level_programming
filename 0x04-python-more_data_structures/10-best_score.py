63ed9e5472f059092d88c0c3da053690fc045b47
0ussamabernou <oussama_bernou@um5.ac.ma>
2023-09-13 18:54:02 +0100

    update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     best = None
     max = 0
-    for k, v in a_dictionary.items:
+    for k, v in a_dictionary.items():
         if v > max:
             max = v
             best = k
