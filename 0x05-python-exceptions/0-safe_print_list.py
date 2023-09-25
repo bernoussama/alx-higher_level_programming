d8571fead8e4c1d3239b7766fa086048785a87fe
0ussamabernou <oussama_bernou@um5.ac.ma>
2023-09-25 14:49:47 +0100

    changed except IndexError to just except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,6 +5,6 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i])
             printed += 1
-        except IndexError:
+        except:
             pass
     return printed
