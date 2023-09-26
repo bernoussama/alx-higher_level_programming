41d76738e1d3b1643f22b12a14bdde1697d90db2
0ussamabernou <oussama_bernou@um5.ac.ma>
2023-09-26 18:36:35 +0100

    add my_print

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Square Module
+"""
+
+
+class Square:
+    """
+    Square Class
+    """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """
+        Returns area of the square
+        """
+        return self.__size**2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def my_print(self):
+        if self.size > 0:
+            print(*["#" * self.__size for _ in range(self.__size)], sep="\n")
+        else:
+            print()
