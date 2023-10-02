7d13e70176c645f6a314d448a6297b5e185fb867
0ussamaBernou <oussama_bernou@um5.ac.ma>
2023-10-02 21:36:45 +0100

    update

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+""" Rectangle Module """
+
+
+class Rectangle:
+    """Rectangle Class"""
+
+    def __init__(self, width=0, height=0):
+        """
+        Initialize new rectangle object
+
+        Args:
+            width: int
+            height: int
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        else:
+            return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        if self.__height == 0 or self.__width == 0:
+            return ""
+        line = "#" * self.__width
+        rect = f"{line}\n" * (self.__height - 1)
+        return rect + line
+
+    def __repr__(self):
+        return f"Rectangle({self.__width},{self.__height})"
