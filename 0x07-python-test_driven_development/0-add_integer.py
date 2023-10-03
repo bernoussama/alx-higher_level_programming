e2e4de0e57e44b906ec3f61a802a82852d3e4476
0ussamaBernou <oussama_bernou@um5.ac.ma>
2023-10-03 22:57:19 +0100

    task 1

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" 
+this is  the "0-add_integer" module.
+
+supplying one function add_integer(). Example,
+
+>>> add_integer(5,5)
+10
+"""
+
+
+def add_integer(a, b=98):
+    """Return the sum of two integers
+
+    Args:
+    a : int|float
+    b : int|float
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
