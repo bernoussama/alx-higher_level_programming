fa5a81679cd684fea5db09426909560a181eb351
0ussamabernou <oussama_bernou@um5.ac.ma>
2023-10-10 20:29:51 +0100

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,6 +9,5 @@ def read_file(filename=""):
     function that reads a text file (UTF8) and prints it to stdout
     """
     with open(filename) as file:
-        print(file)
-        # for line in file:
-        #     print(line)
+        for line in file:
+            print(line)
