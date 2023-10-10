98b61a2a47e965c5bdfee5b863716e05c67ef175
0ussamabernou <oussama_bernou@um5.ac.ma>
2023-10-10 21:27:44 +0100

    update

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+append_write module
+
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    function that appends a string at the end of a text file (UTF8)
+    and returns the number of characters added
+    """
+    with open(filename, "aw") as f:
+        written = f.write(text)
+    return written
