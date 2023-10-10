8bc26be7f8df7fc538a5c1e3a81aaaf8b139e9a6
0ussamabernou <oussama_bernou@um5.ac.ma>
2023-10-10 20:28:44 +0100

    update

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+read_file module
+"""
+
+
+def read_file(filename=""):
+    """
+    function that reads a text file (UTF8) and prints it to stdout
+    """
+    with open(filename) as file:
+        for line in file:
+            print(line)
