054a11c0f86e081c26c1672e072002e3d19f624c
0ussamabernou <oussama_bernou@um5.ac.ma>
2023-10-10 21:58:32 +0100

    update

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+load_from_json_file module
+
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    function that creates an Object from a “JSON file”
+
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        loaded = json.load(f)
+    return loaded
