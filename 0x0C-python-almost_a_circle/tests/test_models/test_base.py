fb82e77f6397aec3c8a78f60fe536000e2bf4644
0ussamabernou <oussama_bernou@um5.ac.ma>
2023-10-19 19:00:00 +0100

    update

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
+"""Unittests for base.py"""
 import unittest
 from models.base import Base
 
 
 class TestBase(unittest.TestCase):
-    def tearDown(self):
-        print("tearing down...")
+    """Unitest for testing Base class"""
 
     def test_autoIncrement(self):
         base = Base()
