a57a4a24428d9fcbf0ba641daa3ad3fad0396a24
0ussamabernou <oussama_bernou@um5.ac.ma>
2023-10-19 18:58:52 +0100

    update

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -4,41 +4,29 @@ from models.base import Base
 
 
 class TestBase(unittest.TestCase):
-    def setUp(self):
-        self.Base = Base
-
     def tearDown(self):
         print("tearing down...")
 
     def test_autoIncrement(self):
-        base = self.Base()
-        b2 = self.Base()
-        b3 = self.Base()
-
-        id = base.id
-        expected = 1
-        self.assertEqual(id, expected, f"wrong id expecting {expected}, got {id}")
+        base = Base()
+        b2 = Base()
 
         id = b2.id
-        expected = 2
-        self.assertEqual(id, expected, f"wrong id expecting {expected}, got {id}")
-
-        id = b3.id
-        expected = 3
+        expected = base.id + 1
         self.assertEqual(id, expected, f"wrong id expecting {expected}, got {id}")
 
     def test_customID(self):
-        b4 = self.Base(12)
+        b4 = Base(12)
 
         id = b4.id
         expected = 12
         self.assertEqual(id, expected, f"wrong id expecting {expected}, got {id}")
 
     def test_autoIncrementAfterCustom(self):
-        b5 = self.Base()
+        b5 = Base()
 
         id = b5.id
-        expected = 4
+        expected = 3
         self.assertEqual(id, expected, f"wrong id expecting {expected}, got {id}")
 
 
