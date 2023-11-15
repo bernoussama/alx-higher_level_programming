7643d8a442e7b309f7126a8ab5865fb2ccc8e735
0ussamabernou <oussama.bernou@outlook.com>
2023-11-16 00:13:21 +0100

    fix first table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -2,7 +2,7 @@
 --  with two columns: id and name
 --  id is an integer and name is a string
 
-CREATE TABLE IF NOT EXISTS first_table (
+CREATE TABLE first_table (
     id INTEGER PRIMARY KEY,
     name varchar(256),
 );
