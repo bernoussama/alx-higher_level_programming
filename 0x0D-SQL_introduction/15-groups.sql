74e22293d9e5d2da4a949ac808399fcd85258752
0ussamabernou <oussama.bernou@outlook.com>
2023-11-16 01:04:23 +0100

    update

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,7 +1,6 @@
--- lists the number of records with the same score in the table second_table of the
--- database hbtn_0c_0
-select score, count(*) as number
-from second_table
-group by score
-order by number desc
+-- lists the number of records with the same score in the table second_table of the database hbtn_0c_0
+SELECT score, count(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number desc
 ;
