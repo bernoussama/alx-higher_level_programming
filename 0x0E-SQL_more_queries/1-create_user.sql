e53c1993d6812dab268a0fc4be7e941ae1335295
0ussamabernou <oussama.bernou@outlook.com>
2023-11-17 02:18:49 +0100

    update

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,6 @@
 -- creates the MySQL server user user_0d_1
 
-CREATE USER 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1';
+CREATE USER 'user_0d_1'@'localhost';
 -- set password
+SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
 
