2fe9345c6330c570c67503f9b2ecbccdb7765b59
0ussamabernou <oussama.bernou@outlook.com>
2023-12-11 22:37:14 +0100

    update

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,5 +4,5 @@ const { argv } = require('node:process');
 const myNumber = Number(argv[2]);
 if (isNaN(myNumber)) console.log('Not a number');
 if (!isNaN(myNumber)) {
-  for (let i = 0; i < myNumber; i++) console.log(`My number: ${myNumber}`);
+  for (let i = 0; i < myNumber; i++) console.log('C is fun');
 }
