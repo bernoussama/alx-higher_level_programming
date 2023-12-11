b71b111ae19aa103217af0aa8b7272c7daf5e4fb
0ussamabernou <oussama.bernou@outlook.com>
2023-12-11 22:16:20 +0100

    update

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-
 const myNumber = Number(argv[2]);
 if (!isNaN(myNumber)) console.log(`My number: ${myNumber}`);
 if (isNaN(myNumber)) console.log('Not a number');
