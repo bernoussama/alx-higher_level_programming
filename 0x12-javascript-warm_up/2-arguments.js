d88b672caff21922f5d2720ccc8cbb288727aaf2
0ussamabernou <oussama.bernou@outlook.com>
2023-12-12 19:55:01 +0100

    update

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-const { argv } = require("node:process");
+const { argv } = require('node:process');
 
 if (argv.length <= 3) {
-  console.log("No argument");
+  console.log('No argument');
 } else if (argv.length === 3) {
-  console.log("Argument found");
+  console.log('Argument found');
 } else if (argv.length > 3) {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }
-
