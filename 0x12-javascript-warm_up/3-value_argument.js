405ef3c6f6cdf45cf31786fb4cd31441c1f2476a
0ussamabernou <oussama.bernou@outlook.com>
2023-12-12 20:37:48 +0100

    update

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const { argv } = require('process');
 
-argv[2] != undefined ? console.log(argv[2]) : console.log('No argument');
+argv[2] !== undefined ? console.log(argv[2]) : console.log('No argument');
