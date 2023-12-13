333027f76681a725bb6416c7664c1719f3c58c97
0ussamabernou <oussama.bernou@outlook.com>
2023-12-14 00:26:17 +0100

    update

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+const Square5 = require('./5-square');
+
+class Square extends Square5 {
+  charPrint (c) {
+    let char = c;
+    if (char === undefined) {
+      char = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(char.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
