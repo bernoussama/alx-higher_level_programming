c890ddffe2daedff4c320c126c542cc0d0003924
0ussamabernou <oussama.bernou@outlook.com>
2023-12-14 01:33:48 +0100

    update

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+
+const adata = read(fileA);
+const bdata = read(fileB);
+
+const cdata = adata + '' + bdata;
+console.log(cdata);
+
+fs.writeFile(fileC, cdata.toString(), 'utf8', function (err) {
+  if (err) {
+    console.error(err);
+  }
+});
+
+function read (filename) {
+  const data = fs.readFileSync(filename, 'utf8');
+  return data;
+}
