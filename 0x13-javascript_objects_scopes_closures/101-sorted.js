a5b13e64a963e21a843cee705b38c558d4c3490e
0ussamabernou <oussama.bernou@outlook.com>
2023-12-14 01:12:14 +0100

    update

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
+
 const newDict = {};
 Object.entries(dict).forEach(([key, value]) => {
   if (newDict[value] === undefined) {
@@ -7,3 +8,5 @@ Object.entries(dict).forEach(([key, value]) => {
   }
   newDict[value].push(key);
 });
+
+console.log(newDict);
