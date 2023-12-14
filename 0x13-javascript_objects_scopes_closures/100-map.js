91a595e15a59f1351f3d1eb5ef0689c62febbda9
0ussamabernou <oussama.bernou@outlook.com>
2023-12-14 01:02:43 +0100

    update

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const list = require('./100-data').list;
+const list = require('./100-data');
 const newlist = list.map((x, index) => x * index);
 console.log(list);
 console.log(newlist);
