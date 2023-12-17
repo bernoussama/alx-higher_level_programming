d6b723aedb582dc48edb295867ccd9bc7568b168
0ussamabernou <oussama.bernou@outlook.com>
2023-12-17 16:48:20 +0100

    update

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,10 +2,11 @@
 """
 class definition of a State and an instance Base = declarative_base()
 """
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-import sys
-import os
+# from sqlalchemy import create_engine
+# import sys
+# import os
 
 
 Base = declarative_base()
@@ -17,13 +18,13 @@ class State(Base):
     name = Column(String(128), nullable=False)
 
 
-if __name__ == "__main__":
-    username = sys.argv[1]
-    pwd = sys.argv[2]
-    host = "localhost" if "ON_MYMACHINE" not in os.environ else "127.0.0.1"
-    db = sys.argv[3]
-
-    engine = create_engine(
-        f"mysql+mysqldb://{username}:{pwd}@{host}/{db}", pool_pre_ping=True
-    )
-    Base.metadata.create_all(engine)
+# if __name__ == "__main__":
+#     username = sys.argv[1]
+#     pwd = sys.argv[2]
+#     host = "localhost" if "ON_MYMACHINE" not in os.environ else "127.0.0.1"
+#     db = sys.argv[3]
+#
+#     engine = create_engine(
+#         f"mysql+mysqldb://{username}:{pwd}@{host}/{db}", pool_pre_ping=True
+#     )
+#     Base.metadata.create_all(engine)
