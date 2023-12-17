ee995a3a411fc30ce5d253bedbfdd48b5404250f
0ussamabernou <oussama.bernou@outlook.com>
2023-12-17 16:13:27 +0100

    update

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
 SELECT cities.id, cities.name, states.name
 FROM cities
 JOIN states ON state_id = states.id
-ORDER BY cities.id ASC"
+ORDER BY cities.id ASC
         """
     )  # HERE I have to know SQL to grab all states in my database
     query_rows = cur.fetchall()
