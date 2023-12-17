3624c277cd87d57fb2c3070a3472b46abb94dec9
0ussamabernou <oussama.bernou@outlook.com>
2023-12-17 15:51:36 +0100

    update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     )
     cur = conn.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC", (name,)
+        f"SELECT * FROM states WHERE name = {name} ORDER BY states.id ASC"
     )  # HERE I have to know SQL to grab all states in my database
     query_rows = cur.fetchall()
     for row in query_rows:
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+""" Script that lists all states from the database hbtn_0e_0_usa """
+
+import MySQLdb
+import sys
+import os
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: {} username password database name".format(sys.argv[0]))
+        exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    name = sys.argv[4]
+    host = "127.0.0.1" if "ON_MYMACHINE" in os.environ else "localhost"
+
+    conn = MySQLdb.connect(
+        host=host,
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+        charset="utf8",
+    )
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC", (name,)
+    )  # HERE I have to know SQL to grab all states in my database
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
