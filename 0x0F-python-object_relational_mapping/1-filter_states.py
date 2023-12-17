c782d08fbd5ca7b24f2a66becf55f9afb9b37399
0ussamabernou <oussama.bernou@outlook.com>
2023-12-17 15:06:36 +0100

    update

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+""" script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa """
+
+import MySQLdb
+import sys
+import os
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    host = "127.0.0.1" if "ON_MYMACHINE" in os.environ else "localhost"
+
+    conn = MySQLdb.connect(
+        host=host,
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+        charset="utf8",
+    )
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT * FROM states ORDER BY states.id ASC"
+    )  # HERE I have to know SQL to grab all states in my database
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
