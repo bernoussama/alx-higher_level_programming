b070d6342b33f72a11658cbec49ef795095dd89b
0ussamabernou <oussama.bernou@outlook.com>
2023-12-17 15:26:50 +0100

    update

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-""" script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa """
+"""
+script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
+"""
 
 import MySQLdb
 import sys
@@ -25,7 +28,9 @@ if __name__ == "__main__":
     )
     cur = conn.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY states.id ASC"
+        """
+SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY states.id ASC
+        """
     )  # HERE I have to know SQL to grab all states in my database
     query_rows = cur.fetchall()
     for row in query_rows:
