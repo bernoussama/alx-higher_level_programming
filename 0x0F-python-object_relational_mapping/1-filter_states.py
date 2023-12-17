0ad5fd83cb935df9d23a4ce6f1ec8c36fd6f391e
0ussamabernou <oussama.bernou@outlook.com>
2023-12-17 15:20:59 +0100

    update

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     )
     cur = conn.cursor()
     cur.execute(
-        "SELECT * FROM states ORDER BY states.id ASC"
+        "SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY states.id ASC"
     )  # HERE I have to know SQL to grab all states in my database
     query_rows = cur.fetchall()
     for row in query_rows:
