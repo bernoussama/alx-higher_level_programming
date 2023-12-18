d004bfeaf2a1ffef25fee916e7e156c922c4f929
0ussamabernou <oussama.bernou@outlook.com>
2023-12-18 01:26:11 +0100

    update

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,1 +1,37 @@
 #!/usr/bin/python3
+"""
+Module that lists all State objects from the database hbtn_0e_14_usa
+"""
+
+import sys
+import os
+from sqlalchemy import create_engine
+from model_state import State
+from model_city import City
+from sqlalchemy.orm import sessionmaker, joinedload
+
+Session = sessionmaker()
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    pwd = sys.argv[2]
+    host = "localhost" if "ON_MYMACHINE" not in os.environ else "127.0.0.1"
+    db = sys.argv[3]
+
+    engine = create_engine(
+        f"mysql+mysqldb://{username}:{pwd}@{host}/{db}",
+        pool_pre_ping=True,  # echo=True
+    )
+    Session.configure(bind=engine)  # once engine is available
+
+    session = Session()
+    for instance in (
+        session.query(City).options(
+            joinedload(City.state_id)).order_by(City.id)
+    ):
+        print(
+            "{}: {} {}".format(
+                instance.state_id,
+                instance.id,
+                instance.name))
+    session.close()
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+class definition of a State and an instance Base = declarative_base()
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+# from sqlalchemy import create_engine
+# import sys
+# import os
+
+
+Base = declarative_base()
+
+
+class City(Base):
+    """
+    class definition of a City
+    """
+
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
