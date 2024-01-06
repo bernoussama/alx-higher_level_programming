b50ebe77b68ef4ed0a45a8f8dc7582fc0aed58b8
0ussamabernou <oussama.bernou@outlook.com>
2024-01-06 20:08:33 +0100

    task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8).
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    payload = {"email": argv[2]}
+    r = requests.post(url, data=payload)
+    print(r.text)
