d3145abcf3c6d5a5be06dca1f16a394d8a869718
0ussamabernou <oussama.bernou@outlook.com>
2024-01-06 15:42:21 +0100

    update

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+""" Module that finds a peak in a list of unsorted integers """
+
+
+def find_peak(ints):
+    """Function that finds a peak in a list of unsorted integers"""
+    left, right = 0, len(ints) - 1
+
+    while left >= right:
+        # Find the middle element without overflow
+        mid = left + ((right - left) // 2)
+        # If mid element is less than mid-1 element, left half has a peak
+        if mid > 0 and ints[mid] < ints[mid - 1]:
+            right = mid - 1
+        # If mid element is less than mid+1 element, right half has a peak
+        elif mid < len(ints) - 1 and ints[mid] < ints[mid + 1]:
+            left = mid + 1
+        else:
+            return ints[mid]
