83281b2a8383e6624611076dd19d8cfbd3d327cd
0ussamabernou <oussama.bernou@outlook.com>
2024-01-06 15:45:30 +0100

    update

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,11 +9,14 @@ def find_peak(ints):
     while left <= right:
         # Find the middle element without overflow
         mid = left + ((right - left) // 2)
+
         # If mid element is less than mid-1 element, left half has a peak
         if mid > 0 and ints[mid] < ints[mid - 1]:
             right = mid - 1
+
         # If mid element is less than mid+1 element, right half has a peak
         elif mid < len(ints) - 1 and ints[mid] < ints[mid + 1]:
             left = mid + 1
+
         else:
             return ints[mid]
