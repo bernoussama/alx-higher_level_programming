74e48bd21ed9d50660743399299adabc734259bb
0ussamabernou <oussama.bernou@outlook.com>
2024-01-06 20:13:46 +0100

    task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""takes in a URL, sends a request to the URL and
+displays the body of the response
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    r = requests.get(url)
+    status = r.status_code
+    print(r.text) if status < 400 else print(f"Error code: {status}")
