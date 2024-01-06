0a3f12c09d701b38966e69aa8cd561fddb8f6e56
0ussamabernou <oussama.bernou@outlook.com>
2024-01-06 20:31:31 +0100

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    param = ""
+    if len(sys.argv) > 1:
+        param = sys.argv[1]
+
+    payload = {"q": param}
+
+    r = requests.get(url, params=payload)
+    try:
+        res = r.json()
+        if len(res) == 0:
+            print("No result")
+        else:
+            id, name = res.get("id"), res.get("name")
+            print(f"[{id}] {name}")
+
+    except requests.exceptions.JSONDecodeError:
+        print("Not a valid JSON")
