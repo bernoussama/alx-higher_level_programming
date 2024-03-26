a4b5c38b5407989886b770a69b0d3fab6d6850c0
0ussamabernou <oussama.bernou@outlook.com>
2024-03-26 22:39:52 +0000

    update

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -9,8 +9,8 @@ const stringToWrite = process.argv[3];
 // Write the string to the file in UTF-8 encoding
 fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
   if (err) {
-    console.error('Error occurred while writing to the file:', err);
+    console.error(err);
   } else {
-    console.log('String written to file successfully.');
+    console.log(stringToWrite);
   }
 });
