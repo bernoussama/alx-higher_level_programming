2d0735ab093d5b2d009fb13266626f5fd788b9e4
0ussamabernou <oussama.bernou@outlook.com>
2024-03-27 18:48:39 +0000

    update

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 
+const { warn } = require('console');
 const fs = require('fs');
 const { exit } = require('process');
 const request = require('request');
@@ -8,23 +9,16 @@ const request = require('request');
 const link = process.argv[2];
 const filePath = process.argv[3];
 
-const content = () => {
-  const html = '';
-  request(link, (err, res, body) => {
-    if (err) {
-      console.error(err);
-      exit();
-    } else {
-      console.log(body);
-      html = body;
-    }
-  });
-  return html;
-};
-
-// Write the string to the file in UTF-8 encoding
-fs.writeFile(filePath, content, 'utf-8', (err) => {
+request(link, (err, res, body) => {
   if (err) {
     console.error(err);
+    exit();
+  } else {
+    // Write the string to the file in UTF-8 encoding
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
   }
 });
