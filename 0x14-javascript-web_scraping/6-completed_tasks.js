bdd5fc518a6e4ae89637a5639c51a23ee1320ed1
0ussamabernou <oussama.bernou@outlook.com>
2024-03-27 18:43:44 +0000

    update

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,13 +4,23 @@ const request = require('request');
 
 const endpoint = process.argv[2];
 
+const todos = {};
 request(endpoint, function (error, response, body) {
   if (error) {
     console.error(error);
   } else {
     const data = JSON.parse(response.body);
     for (const todo of data) {
-      console.log(todo);
+      // console.log(todo);
+      if (todo.completed === true) {
+        const id = todo.userId.toString();
+        if (id in todos) {
+          todos[id] += 1;
+        } else {
+          todos[id] = 1;
+        }
+      }
     }
   }
+  console.log(todos);
 });
