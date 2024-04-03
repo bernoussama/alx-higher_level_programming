cf7ea1406b1349aaa7078db7360338955b15f46a
0ussamabernou <oussama.bernou@outlook.com>
2024-04-03 23:59:21 +0000

    update

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const link = "https://swapi-api.alx-tools.com/api/films/?format=json";
+$.get(link, (data) => {
+  console.log(data);
+  $("UL#list_movies").append(
+    data.results.map((movie) => {
+      return `<li> ${movie.title}</li>`;
+    }),
+  );
+});
