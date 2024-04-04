af2e8177b54b93933e7a14d78c557ff2d457525e
0ussamabernou <oussama.bernou@outlook.com>
2024-04-04 00:11:48 +0000

    update

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+$(document).ready(function () {
+  //
+  const link = "https://hellosalut.stefanbohacek.dev/?lang=fr";
+  $.get(link, (data) => {
+    $("DIV#hello").text(data.hello);
+  });
+});
